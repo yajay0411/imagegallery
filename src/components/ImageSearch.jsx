daf522fb99495322fc8f759fd040253e93684f99
Ajay Yadav <yajay04112000@gmail.com>
2024-03-24 21:20:15 +0530

    enter key down and logo click redirect home

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -6,22 +6,72 @@ const ImageSearch = ({ searchText, searchPage, setImages }) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    searchText(text);
-    searchPage(1);
-    setImages([]);
+    if (!text || text.trim() !== "") {
+      setImages([]);
+      searchPage(1);
+      searchText(text.trim());
+    }
+  };
+
+  const onEnterKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      if (!text || text.trim() !== "") {
+        setImages([]);
+        searchPage(1);
+        searchText(text.trim());
+      }
+    }
+  };
+
+  const resetSearch = (fromHome = false) => {
+    const searchInput = document.getElementById("search-input");
+    searchInput.value = "";
+    setText("");
+
+    if (fromHome) {
+      setImages([]);
+      searchPage(1);
+      searchText("");
+    }
   };
 
   return (
     <>
-      <h1 className="text-purple-600 text-[50px] font-bold">ImageGallery</h1>
-      <form onSubmit={onSubmit} className="flex justify-center items-center">
-        <input
-          type="text"
-          placeholder="Search Images Here"
-          onChange={(e) => setText(e.target.value)}
-          className="text-purple-600 text-[20px] outline-none border-2 border-purple-600
-            p-2 rounded-lg "
-        />
+      <h1
+        className="text-purple-600 text-[50px] font-bold cursor-pointer"
+        onClick={() => {
+          resetSearch(true);
+        }}
+      >
+        ImageGallery
+      </h1>
+      <form
+        onSubmit={onSubmit}
+        onKeyDown={onEnterKeyDown}
+        className="flex justify-center items-center"
+      >
+        <div className="relative flex justify-center items-center">
+          <input
+            id={"search-input"}
+            type="text"
+            placeholder="Search Images Here"
+            onChange={(e) => setText(e.target.value)}
+            className="text-purple-600 text-[20px] outline-none border-2 border-purple-600
+            p-2 rounded-lg"
+          />
+          {!text ||
+            (text.trim() !== "" && (
+              <button
+                className="text-purple-600 font-bold text-lg px-2 absolute right-2"
+                onClick={() => {
+                  resetSearch();
+                }}
+              >
+                X
+              </button>
+            ))}
+        </div>
         <button type="submit">
           <img
             src={search}
